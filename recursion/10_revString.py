23b3ddcecaa384ad63eaf49169a93cae15829a17
data-divaa <divya.1710.drishti@gmail.com>
2025-05-03 19:16:59 +0530

    basic recursion files to understand basics

--- recursion/10_revString.py
+++ recursion/10_revString.py
@@ -4,12 +4,16 @@ Author - Data-Divaa
 Desc - Write a recursive function that returns the reversed version of a string.
 '''
 
-
+#function definition
 def RevStr(string):
+    #base case
     if len(string) == 0:
         return string
+    #recursion call
     return RevStr(string[1:]) + string[0]
 
+#fetching the inputs
 string = input("enter your string :")
 
+#function call
 print(string,"reversed :",RevStr(string))
